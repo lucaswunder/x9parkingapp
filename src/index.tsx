7b39f7957718fa8c96d60c6373cb47b2451f8cfa
lucaswunder <wundervald_2@hotmail.com>
2024-09-22 21:11:46 -0300

    pages

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,17 @@ import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/AuthContext';
 import { BrowserRouter } from 'react-router-dom';
 
+const basename = process.env.PUBLIC_URL
+
+console.log(basename);
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
       <AuthProvider>
         <App />
       </AuthProvider>
